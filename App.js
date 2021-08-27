011116ff63110ea1a73c6200c824e7319c0cbe93
VANSHIKA02-2007 <84086412+VANSHIKA02-2007@users.noreply.github.com>
2021-08-27 23:30:33 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import { View, Button, Alert } from 'react-native';
+
+export default class App extends Component { 
+
+  displayAlert=()=>
+  {
+    alert('The father of computing.');
+  }
+
+ displayAlert1=()=>
+  {
+    alert('The father of nation.');
+  }
+
+displayAlert2=()=>
+  {
+    alert('  The first black president of South Africa.');
+  }
+
+displayAlert3=()=>
+{
+  alert('Saint Teresa of Calcutta.')
+}
+
+  render(){
+    return(
+      <View>
+        <View 
+          style={{ width:200 , height:100, marginTop:60, marginLeft:80}}>
+          <Button 
+            title="Charles Babbage"  
+            color = "red"
+            onPress={this.displayAlert} />
+        </View>
+        
+        <View
+          style={{ width:200 , height:100, marginTop:15, marginLeft:80}}>
+          <Button 
+            title="Mahatma Gandhi"  
+            color = "blue"
+            onPress={this.displayAlert1} />
+        </View>
+
+        <View
+          style={{ width:200 , height:100, marginTop:15, marginLeft:80}}>
+          <Button 
+            title="Nelson Mandela"  
+            color = "purple"
+            onPress={this.displayAlert2} />
+        </View>
+        <View 
+          style={{ width:200 , height:100, marginTop:15, marginLeft:80}}>
+          <Button 
+            title="Mother Teresa"  
+            color = "green"
+            //add code here to show alert box for Mother Teresa button
+            onPress = {this.displayAlert3}
+             />
+        </View>
+      </View>
+    )
+  }
+}
